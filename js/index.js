818e576ba34892e5fd631e426a080e670c27adfe
Mira Vogt <miravogt@gmail.com>
2020-08-10 20:59:29 +0200

    solved Bonus 3

--- js/index.js
+++ js/index.js
@@ -5,3 +5,23 @@
 
 
 // Iteration 3: Loops
+
+// Bonus 2
+let phraseToCheck = "No 'x' in Nixon" 
+
+let spacelessPhrase = ''
+let isPalindrome = false;
+for(let i = 0; i < phraseToCheck.length; i ++) {
+  if(phraseToCheck[i] !== " " && phraseToCheck[i] !== "," && phraseToCheck[i] !== "?" && phraseToCheck[i] !== "!" && phraseToCheck[i] !== "'") {
+    spacelessPhrase += phraseToCheck[i].toLowerCase()
+  }
+}
+console.log(spacelessPhrase)
+for(let i = 0; i < spacelessPhrase.length; i++) {
+  if(spacelessPhrase[i] === spacelessPhrase[spacelessPhrase.length - 1 - i]) {
+    isPalindrome = true;
+  } else {
+    isPalindrome = false;
+    break
+  }
+  }
